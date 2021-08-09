987bef171dff9254a75d2fe44bc375841f9bba6b
bharathkkb <bharathkrishnakb@gmail.com>
2021-08-09 13:54:48 -0500

    eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,5 @@ module.exports = {
       'plugin:@typescript-eslint/eslint-recommended',
       'plugin:@typescript-eslint/recommended',
       'plugin:prettier/recommended',
-      'prettier/@typescript-eslint',
     ],
   };
