3619461f65ffc5d8e5537848b7ec9d109ce78338
Seth Vargo <seth@sethvargo.com>
2021-12-08 19:11:24 -0500

    Add dotfile to integration test (#217)
    * Add dotfile to integration test
    
    * Make unit test look at the actual files instead of just length
    
    * Is the test working now?

--- tests/test-node-func/index.js
+++ tests/test-node-func/index.js
@@ -4,7 +4,17 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
+
+let fs = require('fs');
+
 exports.helloWorld = (req, res) => {
+  // Still send a 200 so we get the response (gaxios and other libraries barf on
+  // non-200)
+  if (!fs.existsSync('.dotfile')) {
+    res.status(200).send('Dotfile does not exist!');
+    return;
+  }
+
   let message = req.query.message || req.body.message || 'Hello World!!';
   res.status(200).send(message);
 };
